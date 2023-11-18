e7c1acbd300c47359ae0c98435113415b44fee25
shoeb <shoebtamboli98@gmail.com>
2023-11-18 22:40:06 +0100

    fixed test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+  test("renders without crashing", () => {
+    render(<App />);
+
+    expect(screen.getByText(/Enjoy on your TV/i)).toBeInTheDocument();
+    expect(screen.getByText(/Watch everywhere/i)).toBeInTheDocument();
+    expect(screen.getByText(/Create profiles for kids/i)).toBeInTheDocument();
+    expect(
+      screen.getByText(/Download your shows to watch offline/i)
+    ).toBeInTheDocument();
+  });
 });
